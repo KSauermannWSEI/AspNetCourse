645a687435a2be0ce1406c33e39a45e0b31b8c12
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2021-04-17 14:28:32 +0200

    View Components

---
+++ AspNetCourse/HairdressingMVC/ViewComponents/PricesCounterViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiClientHelpers;
+
+namespace HairdressingMVC.ViewComponents
+{
+    public class PricesCounterViewComponent : ViewComponent
+    {
+        private WebApiClient<Price> webApiClient;
+        public PricesCounterViewComponent(IConfiguration configuration)
+        {
+            var apiKey = configuration["ApiKeyValue"];
+            webApiClient = new WebApiClient<Price>(
+                configuration.GetConnectionString("ApiBaseUrl"),
+                "Prices",
+                apiKey);
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var list = await webApiClient.GetListAsync();
+            return View(list.Count);
+        }
+    }
+}
